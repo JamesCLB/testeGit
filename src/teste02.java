f15cd2aae36c6a4f3cfcc9fcdcd23bb5c0130ee0
James <jamesdanielxavier@gmail.com>
2023-08-17 16:46:44 -0300

    Meu segundo commiterss

---
+++ src/teste02.java
@@ -0,0 +1,19 @@
+
+public class teste02 {
+	private String name;
+	private String joao;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJoao() {
+		return joao;
+	}
+	public void setJoao(String joao) {
+		this.joao = joao;
+	}
+	
+	
+}
